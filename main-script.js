ac99460b8b09f6e083147511f5493db107c5e271
abdulhaseeebkhalil <abdulhaseeb13095@gmail.com>
2024-09-29 09:55:56 +0500

    DOM elements were added

--- main-script.js
+++ main-script.js
@@ -1,4 +1,12 @@
 var ageCalculatorApp = {
     birthDate: null,
     todayDate: new Date(),
+
+    dataInputEl: document.getElementById('dataInput'),
+    dataPickerEl: document.getElementById('dataPicker'),
+    yearsEl: document.getElementById('years'),
+    monthsEl: document.getElementById('months'),
+    daysEl: document.getElementById('days'),
+    calculateBtnEl: document.getElementById('calculateBtn'),
+    calenderBtnEl: document.getElementById('calenderBtn'),
 }
