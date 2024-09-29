de4e648af19489c5d1737403a5ad73ad30faf4ea
abdulhaseeebkhalil <abdulhaseeb13095@gmail.com>
2024-09-29 10:13:40 +0500

    Calculate age based on birth date and today's date

--- main-script.js
+++ main-script.js
@@ -27,4 +27,51 @@ var ageCalculatorApp = {
             this.calculatAge();
         })
     },   
+
+
+
+
+
+
+
+
+
+    showCalendar(){
+        this.dataPickerEl.style.display = 'block';
+        this.dataPickerEl.focus();
+        this.dataPickerEl.click();
+    },
+
+    handleDataSelection(event){
+        var selectedDate = new Date(event.target.value);
+        if(!isNaN(selectedDate)){
+            this.birthDate = selectedDate;
+            this.dataInputEl.value = selectedDate.toLocaleDateString('en-GB');
+        }
+    },
+
+
+
+    calculatAge(){
+        if (!this.birthDate){
+            alert("Please select a valid birth date.");
+            return;
+        }
+
+        var today = this.todayDate;
+        var ageYears = today.getFullYear() - this.birthDate.getFullYear();
+        var ageMonths = today.getMonth() - this.birthDate.getMonth();
+        var ageDays = today.getDate() - this.birthDate.getDate();
+
+        
+        if(ageDays < 0){
+            ageMonths--;
+            ageDays += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+        }
+        if(ageMonths < 0){
+            ageYears--;
+            ageMonths += 12;
+        }
+        this.updateDom(ageYears, ageMonths, ageDays);
+    },
 }
